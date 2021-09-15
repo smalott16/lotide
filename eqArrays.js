1c02e139aaab75c9a4b781eea8f0c59902ad0225
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-15 00:01:24 +0000

    added eqArrays.js file

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // check if arrays are the same length
+  let areEqual = true;
+  if (array1.length !== array2.length) {
+    areEqual = false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      areEqual = false;
+    }
+  };
+  return areEqual;
+};
+
+//Test Code
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
