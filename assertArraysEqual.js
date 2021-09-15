3154de492d37a63a7186fdf4a8cec07b18fb24d5
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-15 00:33:50 +0000

    add the assertArraysEqual.js file

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function(array1, array2) {
+  // check if arrays are the same length
+  let areEqual = true;
+  if (array1.length !== array2.length) {
+    areEqual = false;
+  }
+  //check each value of array and if any are not matching make it false
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      areEqual = false;
+    }
+  };
+  return areEqual;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Arrays are equal: ${array1} === ${array2}`);
+  } else {
+    console.log(`❌❌❌ Arrays are not equal: ${array1} != ${array2}`);
+  }
+};
+
+assertArraysEqual([1,2,3,4], [1,2,3,4]);
+assertArraysEqual([1,2,3,4], [1,2,3,"4"]);
+assertArraysEqual(["1", "2"], ["1", "2"]);
