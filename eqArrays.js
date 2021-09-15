67ae44326bdaa872546aae6a79a5e29f2d509684
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-15 00:03:43 +0000

    added a new comment to eqArrays file

--- eqArrays.js
+++ eqArrays.js
@@ -12,7 +12,7 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     areEqual = false;
   }
-
+  //check each value of array and if any are not matching make it false
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       areEqual = false;
