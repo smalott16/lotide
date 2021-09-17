268b21ae54b3dba1357aba0a86c2c760f2e56c1f
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-17 00:12:40 +0000

    cleaned up the takeUntil.js file

--- takeUntil.js
+++ takeUntil.js
@@ -25,37 +25,27 @@ const assertArraysEqual = function(array1, array2) {
 //Body of Take Until Function
 const takeUntil = function(array, callback) {
   newArray =[];
-  //loop through array
-  // array.forEach(function(element) {
-  //   //if a negative number is encountered enter this conditional which exits the loop
-  //   if (callback(element)) {
-  //     console.log("found a negative number:", element);
-  //     return newArray;
-  //   }
-  //   console.log("print", element)
-  //   newArray.push(element);
-  // })
-  // return newArray;
-
   for (let i = 0; i < array.length; i++) {
     if (callback(array[i])) {
-      //console.log("found a negative:", array[i]);
       return newArray;
     }
-    //console.log("loops", i);
     newArray.push(array[i]);
   }
   return newArray;
 };
 
 //Test Code
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+// const results1 = takeUntil(data1, x => x < 0);
+// console.log(results1);
 
-const results2 = takeUntil(data1, x => x % 2 === 0);
-console.log(results2);
-assertArraysEqual([1], takeUntil(data1, x => x % 2 === 0));
-assertArraysEqual([1, 2], takeUntil(data1, x => x % 2 === 0));
-assertArraysEqual([1], takeUntil(data1, x => x % 2 !== 0));
-assertArraysEqual([], takeUntil(data1, x => x % 2 !== 0));
+// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+// const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+// const results3 = takeUntil(data1, x => x % 2 === 0);
+// console.log(results3);
+// assertArraysEqual([1], takeUntil(data1, x => x % 2 === 0));
+// assertArraysEqual([1, 2], takeUntil(data1, x => x % 2 === 0));
+// assertArraysEqual([1], takeUntil(data1, x => x % 2 !== 0));
+// assertArraysEqual([], takeUntil(data1, x => x % 2 !== 0));
