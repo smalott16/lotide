bd41f4bd8bba9c107849a2af957466eda43c72ce
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-20 19:58:08 +0000

    refactored the eqArrays to be exportable and added the eqArraysTest file.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} != ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   // check if arrays are the same length
   let areEqual = true;
@@ -21,8 +13,5 @@ const eqArrays = function(array1, array2) {
   return areEqual;
 };
 
-//Test Code
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+module.exports = eqArrays;
+
