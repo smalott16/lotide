d9ca0cd6194b8af9d2703fecdb30d312eb974138
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-20 21:07:29 +0000

    cleaned up the tailTest File. Got rid of commened lines and unused require function calls

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,3 @@
-const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const assert = require('chai').assert;
@@ -20,12 +19,3 @@ describe('#tail', () => {
   });
 
 })
-
-
-// Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// console.log(tail(words)); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// const check = ["Yo Yo"];
-// console.log(tail(check));
