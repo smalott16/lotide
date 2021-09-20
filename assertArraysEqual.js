ae970ecf9b87d3b0ca14ab5e48c6ee2e7eb72236
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-20 20:12:17 +0000

    refactored the assertArraysEqual file to include exports and requires. Moved the test code into the assertArraysEqualTest file.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,4 @@
-const eqArrays = function(array1, array2) {
-  // check if arrays are the same length
-  let areEqual = true;
-  if (array1.length !== array2.length) {
-    areEqual = false;
-  }
-  //check each value of array and if any are not matching make it false
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      areEqual = false;
-    }
-  };
-  return areEqual;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -21,6 +8,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1,2,3,4], [1,2,3,4]);
-assertArraysEqual([1,2,3,4], [1,2,3,"4"]);
-assertArraysEqual(["1", "2"], ["1", "2"]);
+module.exports = assertArraysEqual;
+
