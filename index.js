12edc27b5d05301a34d5f8ed3bf00810fcbc2626
Spencer Malott <spencermalott16@gmail.comgit config --global user.email spencermalott16@gmail.com>
2021-09-20 22:41:23 +0000

    added the ES6 shorthand notation to the lotide object

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const takeUntil = require('./takeUntil');
 const without = require('./without');
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey, 
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map, 
-  takeUntil: takeUntil, 
-  without: without
+  head, 
+  tail,
+  middle,
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map, 
+  takeUntil,
+  without,
 };
